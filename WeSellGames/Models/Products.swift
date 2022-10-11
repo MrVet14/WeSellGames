03b376c8e164a1260b4cb596731686e95e07f319
Vitali <vitali.vyucheiski@gmail.com>
2022-10-11 09:05:35 +0300

    Added appIcon & cleaned up some junk

--- WeSellGames/Models/Products.swift
+++ WeSellGames/Models/Products.swift
@@ -13,8 +13,6 @@ import FirebaseStorage
 
 class Products: ObservableObject {
     @Published private(set) var productList = [Product]()
-    //@Published private(set) var retrievedProductImages = [UIImage]()
-    //@Published private(set) var dicOfRtvPic = [String: Int]()
     @Published var retrievedProductImages = [String: URL]()
     
     func getData() {
@@ -68,7 +66,6 @@ class Products: ObservableObject {
                 if error == nil && url != nil {
                     DispatchQueue.main.async {
                         self.retrievedProductImages[path.replacingOccurrences(of: "images/", with: "").replacingOccurrences(of: ".jpg", with: "")] = url
-                        print("New Entry: \(self.retrievedProductImages)")
                     }
                 }
             }
@@ -76,78 +73,3 @@ class Products: ObservableObject {
     }
     
 }
-
-//self.productList = snapshot.documents.map { d in
-//    let name = d["name"] as? String ?? ""
-//    let image = d["image"] as? String ?? ""
-//    let description = d["description"] as? String ?? ""
-//    let price = d["price"] as? Double ?? 0
-//    //Creating unfiltered string of landscape pictures
-//    let ldImg = d["landImages"] as? String ?? ""
-//    let landImages = ldImg.components(separatedBy: "\\")
-//    // create new item for each doc return
-//    return Product(name: d["name"] as? String ?? "",
-//                   image: d["image"] as? String ?? "",
-//                   landImages: ldImg.components(separatedBy: "\\"),
-//                   description: d["description"] as? String ?? "",
-//                   price: d["price"] as? Double ?? 0)
-//}
-
-//for d in snapshot.documents {
-//    var name = d["name"] as? String ?? ""
-//    var image = d["image"] as? String ?? ""
-//    var description = d["description"] as? String ?? ""
-//    var price = d["price"] as? Double ?? 0
-//
-//    var ldImg = d["landImages"] as? String ?? ""
-//    var landImages = ldImg.components(separatedBy: "\\")
-//
-//    self.productList += [Product(name: name,
-//                            image: image,
-//                            landImages: landImages,
-//                            description: description,
-//                            price: price)]
-//    print("Data parsed")
-//    print(self.productList)
-//}
-
-
-//func getPhotos() {
-//    var numberOfPathEntry = 0
-//
-//    var paths = [String]()
-//    //Getting paths for Images
-//    for path in productList {
-//        paths.append("images/\(path.image).jpg")
-//        for ldPath in path.landImages {
-//            paths.append("images/\(ldPath).jpg")
-//        }
-//    }
-//    print(paths)
-//    //looping through each file path to fetch data
-//    for path in paths {
-//        //Reference to storage
-//        let storageRef = Storage.storage().reference()
-//        //specifying the path
-//        let fileRef = storageRef.child(path)
-//        //Retrieving Data
-//        fileRef.getData(maxSize: 4 * 1024 * 1024) { data, error in
-//            if error == nil && data != nil {
-//                //Creating UIImage and putting it in retrievedProductImages array
-//                if let image = UIImage(data: data!) {
-//                    self.dicOfRtvPic[path] = numberOfPathEntry
-//                    numberOfPathEntry += 1
-//                    //Putting image onto main thread
-//                    DispatchQueue.main.async {
-//                        self.retrievedProductImages.append(image)
-//                        print("Path: \(path)")
-//                        print("New Entry: \(self.retrievedProductImages)")
-//                    }
-//                    print("dic value:\(self.dicOfRtvPic)")
-//                }
-//            } else {
-//                print(error ?? "")
-//            }
-//        }
-//    } //End Path loop
-//}
